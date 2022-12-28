506ab8bf7f0b8f3b7694b9af13cc70bf2a34b5c5
Rafaela Freitas <rafaelathaisff@gmail.com>
2022-12-28 17:48:21 -0300

    add rtc files

---
+++ Src/010rtc-lcd.c
@@ -0,0 +1,13 @@
+/*
+ * 010etc-lcd.c
+ *
+ *  Created on: 28 de dez de 2022
+ *      Author: r.freitas
+ */
+
+
+extern void initialise_monitor_handles(void);
+
+int main (){
+	initialise_monitor_handles();
+}
